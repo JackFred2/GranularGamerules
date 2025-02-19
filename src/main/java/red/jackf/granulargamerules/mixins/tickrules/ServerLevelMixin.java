cbe273e2a83f467695843d57f682e09bc681cf08
Jack Pearce <jack@jackf.red>
2025-02-19 04:03:22 +0000

    [internal] 1.21.1 changes

--- src/main/java/red/jackf/granulargamerules/mixins/tickrules/ServerLevelMixin.java
+++ src/main/java/red/jackf/granulargamerules/mixins/tickrules/ServerLevelMixin.java
@@ -11,8 +11,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.profiling.Profiler;
-import net.minecraft.world.level.GameRules;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.LeavesBlock;
@@ -22,19 +21,19 @@ import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import red.jackf.granulargamerules.impl.rules.TickRules;
 
+import java.util.function.Supplier;
+
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin extends Level {
-    protected ServerLevelMixin(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, RegistryAccess registryAccess, Holder<DimensionType> holder, boolean bl, boolean bl2, long l, int i) {
-        super(writableLevelData, resourceKey, registryAccess, holder, bl, bl2, l, i);
-    }
 
-    @Shadow public abstract GameRules getGameRules();
+    protected ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, Supplier<ProfilerFiller> profiler, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+        super(levelData, dimension, registryAccess, dimensionTypeRegistration, profiler, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
+    }
 
     @WrapOperation(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunkSection;isRandomlyTicking()Z"))
     private boolean tickrules$captureLevelSection(LevelChunkSection instance, Operation<Boolean> original, @Share("chunkSection") LocalRef<LevelChunkSection> chunkSection) {
@@ -60,7 +59,7 @@ public abstract class ServerLevelMixin extends Level {
 
         for(int i = 0; i < TickRules.getMaxExtraTicks(this.getGameRules()); i++) {
             BlockPos pos = this.getBlockRandomPos(minX, minY, minZ, 15);
-            Profiler.get().push("ggRandomTick");
+            this.getProfiler().push("ggRandomTick");
             BlockState blockState = chunkSection.get().getBlockState(pos.getX() - minX, pos.getY() - minY, pos.getZ() - minZ);
 
             if (blockState.isRandomlyTicking()) {
@@ -69,8 +68,8 @@ public abstract class ServerLevelMixin extends Level {
                     blockState.randomTick((ServerLevel) (Object) this, pos, this.random);
                 }
             }
-            
-            Profiler.get().pop();
+
+            this.getProfiler().pop();
         }
     }
 }
